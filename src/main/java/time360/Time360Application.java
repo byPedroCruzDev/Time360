8035468276f5953b7919f061fca3e9b8db029800
PedroCruz <contatocreed67@gmail.com>
2023-10-17 20:26:03 -0400

    restructuring project

---
+++ src/main/java/time360/Time360Application.java
@@ -0,0 +1,13 @@
+package time360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Time360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Time360Application.class, args);
+	}
+
+}
